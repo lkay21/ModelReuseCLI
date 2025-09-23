be7ac9c8d4561fe739c8c28fbc25bfc831af3715
golovenchits <golovenchitc@gmail.com>
2025-09-23 12:40:43 -0400

    refined prompt

--- metrics/performance_claims.py
+++ metrics/performance_claims.py
@@ -8,8 +8,11 @@ def performance_claims(model_id: str) -> float:
     model_card = client.model_card_text(model_id)
     res = prompt_gemini(f"Read the following model card of a HuggingFaceModel. " \
                     "Evaluate on a float scale of 0 to 1 the claimed performance of the model. " \
-                    "Assign 0.33 points if there is benchmarking data present and another 0.33 points if there are testing scores present and another 0.34 for other performance claims." \
-                    "The last line of your response must only contain the final score as a float. You may give partial credit on the last point, depending on the extensiveness of the claims." \
+                    "Assign 0.33 points if there is benchmarking data present " \
+                    "and another 0.33 points if there are testing scores present "\
+                    ", add another 0.1 points for presence of other performance claims." \
+                    "and another 0.24 if the card contains specific and data-backed claims (partial credit is allowed for this 0.24)" \
+                    "The last line of your response must only contain the final score as a float." \
                     "Reply only with a float in the format x.xx" \
                     f"{model_card}", get_gemini_key())
     try:
