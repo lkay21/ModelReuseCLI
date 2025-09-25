054542fdb73213d0ef0d9c704327375f9c0fd037
vdudhaiy <vdudhaiy@purdue.edu>
2025-09-25 13:05:47 -0400

    purdue gen ai set up (#45)

---
+++ utils/prompt_key.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+from typing import Dict
+from apis.gemini import get_gemini_key
+from apis.purdue_genai import get_purdue_genai_key
+
+
+logger = logging.getLogger('cli_logger')
+
+
+def get_prompt_key() -> Dict[str, str]:
+    """
+    Obtain prompting API key - either Purdue GenAI Studio or Gemini.
+
+    Returns:
+        key (Dict): Dictionary containing the API key
+    """
+    logger.info("Compiling API key from available sources...")
+    keys = {}
+
+    # Purdue GenAI Studio and Gemini
+    purdue_genai_token = get_purdue_genai_key()
+    gemini_api_key = get_gemini_key()
+    if purdue_genai_token:
+        return {"purdue_genai": purdue_genai_token}
+    elif gemini_api_key:
+        return {"gemini": gemini_api_key}
+    
+    if not purdue_genai_token and not gemini_api_key:
+        logger.error("No API keys found for Gemini or Purdue GenAI Studio. Exiting.")
+        sys.exit(-1)
