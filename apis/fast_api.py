c9e0a16413209cba8a6042dc5bc036894b994581
Logan Charles Kay <kay21@eceprog1.ecn.purdue.edu>
2025-11-29 20:02:20 -0500

    attemp at fixing single query error

--- apis/fast_api.py
+++ apis/fast_api.py
@@ -152,68 +152,51 @@ async def find_artifacts(x_authorization: str = Header(None), queries: List[Arti
     if not queries or any(not query.name for query in queries):
         raise HTTPException(status_code=400, detail="error in request body")
 
-    if len(queries) == 1 and queries[0].name == "*":
-        try:
-            scan = model_table.scan()
 
-            for item in scan['Items']:
+    for query in queries:
+        name = query.name
+        if name == "*":
+            try:
+                scan = model_table.scan()
 
-                artifact = {
-                    "name": item.get("name"),
-                    "id": item.get("model_id"),
-                    "type": item.get("type")
-                }
+                for item in scan['Items']:
 
-                artifacts.append(artifact)
+                    artifact = {
+                        "name": item.get("name"),
+                        "id": item.get("model_id"),
+                        "type": item.get("type")
+                    }
 
-        except Exception as e:
-            raise HTTPException(status_code=403, detail=f"Failed to retrieve artifacts: {e}")
+                    artifacts.append(artifact)
+                
+                break
+            except Exception as e:
+                raise HTTPException(status_code=403, detail=f"Failed to retrieve artifacts: {e}")
+        else:     
+            try: 
+                response = model_table.query(
+                    IndexName = "name",
+                    KeyConditionExpression="name = :nameValue",
+                    ExpressionAttributeValues={
+                        ":nameValue": {"S": name} 
+                    }
+                )
 
-    else:
-        # for query in queries:
-        #     name = query.name
-        #     if name == "*":
-        #         try:
-        #             scan = model_table.scan()
+                items = response.get('Items', [])
 
-        #             for item in scan['Items']:
+                for item in items:
+                    if item.get("type") not in query.types and query.types is not None:
+                        continue
+                    else:
+                        artifact = {
+                            "name": item.get("name"),
+                            "id": item.get("model_id"),
+                            "type": item.get("type")
+                        }
+                        artifacts.append(artifact)
 
-        #                 artifact = {
-        #                     "name": item.get("name"),
-        #                     "id": item.get("model_id"),
-        #                     "type": item.get("type")
-        #                 }
-
-        #                 if item.get("type") in query.types:
-        #                     artifacts.append(artifact)
-
-        #         except Exception as e:
-        #             raise HTTPException(status_code=403, detail=f"Failed to retrieve artifacts: {e}")
-        #     try: 
-        #         response = model_table.query(
-        #             IndexName = "name",
-        #             KeyConditionExpression="name = :nameValue",
-        #             ExpressionAttributeValues={
-        #                 ":nameValue": {"S": name} 
-        #             }
-        #         )
-
-        #         items = response.get('Items', [])
-        #         for item in items:
-        #             if item.get("type") not in query.types and query.types is not None:
-        #                 continue
-        #             else:
-        #                 artifact = {
-        #                     "name": item.get("name"),
-        #                     "id": item.get("model_id"),
-        #                     "type": item.get("type")
-        #                 }
-        #                 artifacts.append(artifact)
-
-        #     except Exception as e:
-        #         raise HTTPException(status_code=403, detail=f"Failed to retrieve artifacts: {e}")
-            
-        return "multiple queries commented out"
+            except Exception as e:
+                raise HTTPException(status_code=403, detail=f"Failed to retrieve artifacts: {e}")
     
     return artifacts
 
