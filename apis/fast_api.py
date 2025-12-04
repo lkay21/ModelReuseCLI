94353fae2e43dcc9c9ecd47cc160736675cc62fd
Syed Yahya Quadri <quadris@eceprog5.ecn.purdue.edu>
2025-12-03 22:49:29 -0500

    Changing -ID to throw diff error

--- apis/fast_api.py
+++ apis/fast_api.py
@@ -362,7 +362,8 @@ async def read_artifact(artifact_type: str, id: str, x_authorization: str = Head
     try:
         model_id = int(id)
         if model_id <= 0:
-            raise ValueError()
+            # raise ValueError()
+            raise HTTPException(status_code=404, detail="There is missing field(s) in the artifact_type or artifact_id or it is formed improperly, or is invalid.")
     except ValueError:
         raise HTTPException(status_code=400, detail="There is missing field(s) in the artifact_type or artifact_id or it is formed improperly, or is invalid.")
 
